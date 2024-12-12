92f49a4b1362cd6e483c99d1c9ca41af715924c7
Madhusudhan Srinivasa <madhu@Madhusudhans-MacBook-Air.local>
2024-12-12 15:20:21 -0600

    Updates

---
+++ precache-manifest.d2017fde2b92560725b57c1ca364c906.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d07d7a1550cc76f38e47b3f607b6038",
+    "url": "/icpl-platform/index.html"
+  },
+  {
+    "revision": "33a5fa3b88e698f69ed0",
+    "url": "/icpl-platform/static/css/main.bfdc4dec.chunk.css"
+  },
+  {
+    "revision": "7929f3d584ebb05fae61",
+    "url": "/icpl-platform/static/js/2.05b96f21.chunk.js"
+  },
+  {
+    "revision": "33a5fa3b88e698f69ed0",
+    "url": "/icpl-platform/static/js/main.088f91be.chunk.js"
+  },
+  {
+    "revision": "cbc553ab944e68d885d4",
+    "url": "/icpl-platform/static/js/runtime~main.2452e2a6.js"
+  },
+  {
+    "revision": "4d761b59f562ebb4fe1bdf06933cf22d",
+    "url": "/icpl-platform/static/media/logo.4d761b59.png"
+  },
+  {
+    "revision": "aa35f73868124469233b007d674059bd",
+    "url": "/icpl-platform/static/media/monkey-video.aa35f738.mp4"
+  },
+  {
+    "revision": "c876728e652805c07ed60493df5bea30",
+    "url": "/icpl-platform/static/media/monkey.c876728e.png"
+  }
+]);
